9dd0901e6806808b705e7ebfca380a015bf7aaf1
Donato Di Ferdinando <fddiferd@gmail.com>
2025-12-15 09:19:58 -0800

    commit

--- app/src/utils/typing.py
+++ app/src/utils/typing.py
@@ -1,14 +1,62 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import Union, List, Any
+import logging
+
 from src.constants import (
     BI_LAYER_DB,
     BI_LAYER_SCHEMA
 )
 
-def get_sql_str(table_name: str, sub_tree_id: str | None = None) -> str:
-    return f"""
+
+logger = logging.getLogger(__name__)
+
+
+class SqlOperator(Enum):
+    EQUALS = "="
+    NOT_EQUALS = "!="
+    GREATER_THAN = ">"
+    LESS_THAN = "<"
+    GREATER_THAN_OR_EQUAL_TO = ">="
+    LESS_THAN_OR_EQUAL_TO = "<="
+    LIKE = "LIKE"
+    NOT_LIKE = "NOT LIKE"
+
+class SqlWhere(BaseModel):
+    key: str
+    operator: SqlOperator
+    value: Any
+
+    def to_sql(self) -> str:
+        is_value_string = isinstance(self.value, str)
+        if is_value_string:
+            return f"{self.key} {self.operator.value} '{self.value}'"
+        else:
+            return f"{self.key} {self.operator.value} {self.value}"
+
+SqlWhereStatement = Union[SqlWhere, List[SqlWhere]]
+
+
+def get_sql_str(table_name: str, where: SqlWhereStatement | None = None) -> str:
+    where_clause = ""
+    if where is not None:
+        if isinstance(where, list):
+            if where:  # Ensure list is not empty
+                conditions = [w.to_sql() for w in where]
+                where_clause = f" WHERE {' AND '.join(conditions)}"
+        else:
+            where_clause = f" WHERE {where.to_sql()}"
+
+   
+    sql: str = f"""
         select
         *
         from {BI_LAYER_DB}.{BI_LAYER_SCHEMA}.{table_name}
-        """ + (f"where sub_tree_id= '{sub_tree_id}'" if sub_tree_id else "")
+        {where_clause}
+    """ 
+    logger.debug(f"sql: {sql}")
+    return sql
+
 
 def get_version(version_number: int) -> str:
     return f'V{version_number}'
